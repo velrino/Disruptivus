2ccbf9ad5303c9b5ca617ae89e4d47d93ab697b0
velrino <denismagalhaesvelrino@gmail.com>
2020-11-30 17:25:19 -0300

    :zap: implemented gallery

--- src/app/pages/scenes/scenes.component.ts
+++ src/app/pages/scenes/scenes.component.ts
@@ -6,7 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scenes.component.scss']
 })
 export class ScenesComponent implements OnInit {
-
+  book = [
+    {
+      chapter: 'prólogo',
+      imgs: [
+        {
+          src: '/assets/scenes/1 - Monike Lab.png',
+          text: null
+        },
+        {
+          src: '/assets/scenes/1 - Monike Lab 2.png',
+          text: null
+        }
+      ]
+    },
+    {
+      chapter: 'zaíra',
+      imgs: [
+        {
+          src: '/assets/scenes/2 - Naeli office.png',
+          text: null
+        },
+        {
+          src: '/assets/scenes/2 - Zaira room.png',
+          text: null
+        }
+      ]
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
