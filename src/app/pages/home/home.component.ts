53a3af093a3b4e67ebc282213aaf9c41051cf96b
velrino <denismagalhaesvelrino@gmail.com>
2020-11-30 14:38:14 -0300

    :art: installed fontawesome

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  socials = [
+    {
+      class: 'fab fa-facebook-square fa-3x',
+      link: 'https://facebook.com/disruptivus'
+    },
+    {
+      class: 'fab fa-instagram fa-3x',
+      link: 'https://instagram.com/disruptivus'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
